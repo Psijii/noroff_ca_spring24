69a97fa107afe2d251a3a40ae57ce8fda44da847
Siw Elin Årolilja Iversen <111748314+Psijii@users.noreply.github.com>
2024-05-03 15:09:57 +0200

    setting up api folder

---
+++ holidaze-ca/src/api/Constants.jsx
@@ -0,0 +1,4 @@
+export const API_BASE_URL = "https://v2.api.noroff.dev/";
+export const API_BASE_Version = "/api/v1";
+export const API_SOCIAL_BASE = "/holidaze";
+export const API_SOCIAL_URL = `${API_BASE_URL}${API_BASE_Version}${API_SOCIAL_BASE}`;
