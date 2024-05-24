269980c488e8229cd6751f5af4ea40ca3c0ac855
Siw Elin Årolilja Iversen <111748314+Psijii@users.noreply.github.com>
2024-05-24 11:17:21 +0200

    pushing dodcs

--- src/hooks/useSingleProfile/index.jsx
+++ src/hooks/useSingleProfile/index.jsx
@@ -1,7 +1,16 @@
+
+
+
 import { useEffect, useState, useMemo } from "react";
 import { authFetch } from "../../helper/authorization";
 import { PROFILE_URL } from "../../constants/index.jsx";
 
+/**
+ * Custom hook for fetching a single profile.
+ *
+ * @param {string} name - The name of the profile to fetch.
+ * @returns {Object} - An object containing the single profile, loading state, error state, and error details.
+ */
 export function useSingleProfile(name) {
   const url = useMemo(() => {
     return `${PROFILE_URL}${name}?_bookings=true&_venues=true&sortOrder=desc&sort=created`;
@@ -12,6 +21,11 @@ export function useSingleProfile(name) {
   const [isError, setIsError] = useState(false);
   const [errorDetails, setErrorDetails] = useState("");
 
+  /**
+   * Fetches the single profile from the provided URL.
+   *
+   * @param {string} fetchUrl - The URL to fetch the single profile from.
+   */
   async function getSingleProfile(fetchUrl) {
     try {
       setIsLoading(true);
